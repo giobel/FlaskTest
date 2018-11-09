fbfa6a5bfe2e2273de4fa778f00dd7e14e18ccbc
GLOBAL\Giovanni.Brogiolo <giovanni.brogiolo>
2018-11-09 23:20:05 +1100

    added drg register

--- app/app.py
+++ app/app.py
@@ -2,6 +2,8 @@ from flask import Flask, flash, redirect, render_template, request, send_from_di
 import os
 from datetime import datetime
 import re
+import csv
+
 
 app = Flask(__name__)
 
@@ -14,6 +16,7 @@ def home():
 @app.route('/sheets')
 def sheets():
     formatted_now = datetime.now().strftime("%d/%m/%y")
+    object_list = get_csv()
     sheets = [{"Revision A":["105", "100", "106"]},{"Revision B":["230", "260", "261"]},{"Revision C":["310","311","312"]}]
     image_names = os.listdir('./images')
     return render_template('sheets.html', sheets = sheets, date = formatted_now, imageNames = image_names)
@@ -30,6 +33,19 @@ def getGallery():
 @app.route("/upload/<filename>")
 def send_image(filename):
     return send_from_directory("images", filename)
+
+def get_csv():
+    csv_path = './static/projectDrg.csv'
+    csv_file = open(csv_path, 'r')
+    csv_obj = csv.DictReader(csv_file)
+    csv_list = list(csv_obj)
+    return csv_list
+
+@app.route("/drawingRegister")
+def drawingRegister():
+    object_list = get_csv()
+    return render_template('drawingRegister.html', object_list=object_list)
+
 
 if __name__ == "__main__":
     app.run()
--- app/test.py
+++ app/test.py
@@ -1,4 +1,6 @@
 import os
+import collections
+import csv
 
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 
@@ -6,4 +8,29 @@ target = os.path.join(APP_ROOT, 'images/')
 
 image_names = os.listdir(target)
 
-print (image_names[0][41:44])
+
+def get_csv():
+    csv_path = './static/la-riots-deaths.csv'
+    csv_file = open(csv_path, 'r')
+    csv_obj = csv.DictReader(csv_file)
+    csv_list = list(csv_obj)
+    return csv_list
+
+d = get_csv()
+keys = []
+dt = []
+items = []
+values = []
+
+for item in d:
+    items.append(item)
+    subVal = []
+    values.append(subVal)
+    for k, v in item.items():
+        keys.append(k)
+        subVal.append(v)
+
+t = [list(x) for x in zip(*values)]
+
+for obj in d:
+    print(obj.full_name)
