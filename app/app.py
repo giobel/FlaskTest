eb02d859d07fb0c6ecf301fe6fcbb5417bb3fc67
GLOBAL\Giovanni.Brogiolo <giovanni.brogiolo>
2018-11-10 18:12:40 +1100

    updated sheet by revision

--- app/app.py
+++ app/app.py
@@ -3,7 +3,7 @@ import os
 from datetime import datetime
 import re
 import csv
-
+from collections import defaultdict
 
 app = Flask(__name__)
 
@@ -17,9 +17,10 @@ def home():
 def sheets():
     formatted_now = datetime.now().strftime("%d/%m/%y")
     object_list = get_csv()
-    sheets = [{"Revision A":["105", "100", "106"]},{"Revision B":["230", "260", "261"]},{"Revision C":["310","311","312"]}]
+    #sheets = [{"Revision A":["105", "100", "106"]},{"Revision B":["230", "260", "261"]},{"Revision C":["310","311","312"]}]
+    items = massageCsv('./static/projectDrg.csv')
     image_names = os.listdir('./images')
-    return render_template('sheets.html', sheets = sheets, date = formatted_now, imageNames = image_names)
+    return render_template('sheets.html', items = items, date = formatted_now, imageNames = image_names)
 
 @app.route('/model')
 def model():
@@ -45,6 +46,24 @@ def get_csv():
 def drawingRegister():
     object_list = get_csv()
     return render_template('drawingRegister.html', object_list=object_list)
+
+
+
+def massageCsv(filePath):
+        csv_path = filePath
+        csv_file = open(csv_path, 'r')
+        csv_obj = csv.DictReader(csv_file)
+        revisions = []
+        sheets = []
+        for s in csv_obj:
+                revisions.append(s['Rev4'])
+                sheets.append(s['Drg_No'])
+        sheetByRevision = dict(zip(sheets, revisions))
+        output = defaultdict(list)
+        for key, value in sorted(sheetByRevision.items()):
+                output[value].append(key)
+        return output
+
 
 
 if __name__ == "__main__":
--- app/test.py
+++ app/test.py
@@ -1,6 +1,7 @@
 import os
 import collections
 import csv
+from collections import defaultdict
 
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 
@@ -32,5 +33,74 @@ for item in d:
 
 t = [list(x) for x in zip(*values)]
 
-for obj in d:
-    print(obj.full_name)
+
+def get_dict():
+    csv_path = './static/projectDrg.csv'
+    csv_file = open(csv_path, 'r')
+    csv_obj = csv.DictReader(csv_file)
+    return csv_obj
+
+
+dic = get_dict()
+
+sheetByRevision = {}
+
+revisions = []
+sheets = []
+
+for s in dic:
+        revisions.append(s['Rev4'])
+        sheets.append(s['Drg_No'])
+
+
+sheetByRevision = dict(zip(sheets,revisions))
+
+
+
+v = defaultdict(list)
+
+for key, value in sorted(sheetByRevision.items()):
+    v[value].append(key)
+
+
+def massageCsv(filePath):
+        csv_path = filePath
+        csv_file = open(csv_path, 'r')
+        csv_obj = csv.DictReader(csv_file)
+        revisions = []
+        sheets = []
+        for s in csv_obj:
+                revisions.append(s['Rev4'])
+                sheets.append(s['Drg_No'])
+        sheetByRevision = dict(zip(sheets, revisions))
+        output = defaultdict(list)
+        for key, value in sorted(sheetByRevision.items()):
+                output[value].append(key)
+        return output
+
+vi = massageCsv('./static/projectDrg.csv')
+
+for k in vi:
+        #print (k)
+        for v in k:
+                for sheet in vi[k]:
+                        continue
+                        #print (sheet)
+
+for rev in items:
+        print (rev)
+
+
+sheets = [{"Revision A":["105", "100", "106"]},{"Revision B":["230", "260", "261"]},{"Revision C":["310","311","312"]}]
+
+keys = ['a','b','c']
+
+sh = [['105','106'],['201','202'],['301','350']]
+
+dictionar = {}
+
+for i in range(0,len(keys)):
+        for j in range(0,len(sh[i])):
+                dictionar.setdefault(keys[i],[]).append(sh[i][j])
+
+print(vi)
